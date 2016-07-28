6c578a626c2da2e0e95b9910bba9409c04215cc2
nepeat <nepeat@users.noreply.github.com>
2016-07-28 04:39:14 -0800

    Decode Redis responses if told to do so.

--- worker/worker/connections.py
+++ worker/worker/connections.py
@@ -4,11 +4,11 @@ from cassandra.cluster import Cluster
 from redis import StrictRedis
 
 
-def create_redis() -> StrictRedis:
+def create_redis(decode_responses=False) -> StrictRedis:
     return StrictRedis(
         host=os.environ.get("REDIS_HOST", "localhost"),
         port=os.environ.get("REDIS_PORT", 6379),
-        decode_responses=True,
+        decode_responses=decode_responses
     )
 
 def create_cassandra() -> Cluster:
